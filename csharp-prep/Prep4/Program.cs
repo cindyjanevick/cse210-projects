3593b4750745bbb5ee38d72c6e4e0c66d01846d6
cindyjanevick <calarco@byui.edu>
2024-01-14 02:07:34 +0900

    prep4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int user_number = -1;
+        while (user_number !=0)
+        {
+            Console.Write("Enter a number (write 0 to quit): ");
+
+            string user_answer = Console.ReadLine();
+            user_number = int.Parse(user_answer);
+
+            if (user_number !=0)
+            {
+                numbers.Add(user_number);
+            }
+        }
+
+        int sum = 0;
+        foreach (int number in numbers)
+        { 
+            sum += number;
+
+        }
+        Console.WriteLine($"The sum is: {sum}");
+        float average = ((float)sum) / numbers.Count;
+        Console.WriteLine($"The average is: {average}");
+
+        int max = numbers[0];
+
+        foreach (int number in numbers)
+        {
+            if (number > max)
+            {
+                max = number;
+            }
+        }
+        Console.WriteLine($"The max is: {max}");
+        
     }
 }
