d01156e0ee4b05124e48c13a84ba59fdfd9de72f
cindyjanevick <calarco@byui.edu>
2024-01-14 01:56:20 +0900

    prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.Write("What is the magic number? ");
+        //string userinput = Console.ReadLine();
+        //int number = int.Parse(userinput);//
+
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+        
+       int guess = 1;
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess =int.Parse(Console.ReadLine());
+
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
